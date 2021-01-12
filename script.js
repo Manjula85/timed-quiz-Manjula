0f028ccde3a35f4e26b87845336573c2db42ed94
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-01-11 21:53:28 -0500

    added all the questions and the result at the bottom

--- script.js
+++ script.js
@@ -61,22 +61,64 @@ var multiSolutions = document.querySelector("#center");
 //Where the buttons with the answers go
 var listLocation = document.querySelector("#allOptions")
 
-//Checking if the right answer was selected
-var result = 0;
+//Keeping track of the questions
+var nextQuestion = 0;
 
-//identifying buttons
+//So that I can show it at the bottom of the questions
+var quizResult = 0;
+
+//checking solution
+var checkingSolution = function(event){
+
+    // get target element from event
+    var targetEl = event.target.id;
+    targetEl = parseInt(targetEl);
+
+    //console.log("target: " + targetEl);
+    //console.log("quiz answer: " + quizAnswer);
+    
+    if(targetEl === quizAnswer){
+        quizResult = "Correct!";
+        nextQuestion++;   //Add to the total score
+        //alert("here");
+    }else{
+        quizResult = "Wrong!"
+    }
+
+    //remove line (so there aren't multiples of them).
+    line.remove();
+    resOutput.remove();
+
+
+    //Loading the second question here
+    var allQuestions = [firstQuestion,secondQuestion,thirdQuestion,fourthQuestion,fifthQuestion];
+    //to loop through each question and get the final total
+    //i=1 because the first question has already been asked
+
+    //removing the existing buttons first
+    if(nextQuestion <= allQuestions.length){
+        continueQuestions(allQuestions[nextQuestion]);
+    }else{
+        //meaning it has reached the end go to scoring
+    }
+
+    //showing the quizResult of the prevous page at the bottom 
+};
 
 var startQuiz = function(studentQuestion){   
 
+    //Keeping track of the questions
+    nextQuestion++;
+
     //question
-    var question = document.getElementById("main-content");  //h1 element
+    var question = document.getElementById("main-content");  //middle div
     question.textContent = studentQuestion.question;
 
     //multiple choice
     var listEl = document.createElement("li");
     listEl.classList = "list-group";
 
-    event.preventDefault();
+    //event.preventDefault();
 
     //Q1
     var buttonOne = document.createElement("button");
@@ -110,48 +152,76 @@ var startQuiz = function(studentQuestion){
     listEl.appendChild(buttonFour);
     listLocation.appendChild(listEl);
 
+    //Pass correct answer
+    quizAnswer = studentQuestion.correctAns;
 
     //button click event 
-    buttonOne.addEventListener("click",taskButtonHandler);
-    buttonTwo.addEventListener("click",taskButtonHandler);
-    buttonThree.addEventListener("click",taskButtonHandler);
-    buttonFour.addEventListener("click",taskButtonHandler); 
-
+    buttonOne.addEventListener("click",checkingSolution);
+    buttonTwo.addEventListener("click",checkingSolution);
+    buttonThree.addEventListener("click",checkingSolution);
+    buttonFour.addEventListener("click",checkingSolution); 
 };
 
-var taskButtonHandler = function(event) {
-    // get target element from event
-    var targetEl = event.target;
+//Horizontal line
+var line = document.createElement("hr");
+line.className = "hrStyle";
 
-    alert(targetEl.id);
- }
+var resOutput = document.createElement("li");
+resOutput.className = "preResult";
+
+//the rest of the quesitons
+var continueQuestions = function(studentQuestion,lastAns){
+
+    //Keeping track of the questions
+    nextQuestion++;    
+
+    //question
+    var question = document.getElementById("main-content");  //h1 element
+    question.textContent = studentQuestion.question;
+
+    //Q1
+    var buttonOne = document.getElementById(1);
+    buttonOne.textContent = studentQuestion.ansOne;
+
+    //Q2    
+    var buttonTwo = document.getElementById(2);
+    buttonTwo.textContent = studentQuestion.ansTwo;
+
+    //Q3
+    var buttonThree = document.getElementById(3);
+    buttonThree.textContent = studentQuestion.ansThree;
+
+    //Q4
+    var buttonFour = document.getElementById(4);
+    buttonFour.textContent = studentQuestion.ansFour;
+
+    //************************** */
+    //The solution from the previous question
+
+    //multiple choice
+    var listEl = document.createElement("li");
+    listEl.classList = "list-group";
+    
+    //Horizontal line creation
+    listEl.appendChild(line);
+    listLocation.appendChild(listEl);
+
+    //result
+    resOutput.textContent = quizResult;
+    listEl.appendChild(resOutput);
+    listLocation.appendChild(listEl);
   
 
-//checking solution
-var checkingSolution = function(studentAnswer){
+    //************************** */
 
-    //event.preventDefault();
-    //Loading the second question here
+    //Pass correct answer
+    quizAnswer = studentQuestion.correctAns;
 
-    //var allQuestions = [firstQuestion,secondQuestion,thirdQuestion,fourthQuestion,fifthQuestion];
-    //to loop through each question and get the final result
-    //i=1 because the first question has already been asked
-/*
-    for(var i=1; i<allQuestions.length; i++){
-        startQuiz(i);
-    };
-*/
-    var quizResult;
-      
-    /*
-    if(parseInt(output) === studentAnswer.correctAns){
-        quizResult = "Correct!";
-        result++;   //Add to the total score
-    }else{
-        quizResult = "Wrong!"
-    }*/
-
-    //showing the result of the prevous page at the bottom 
+    //button click event 
+    buttonOne.addEventListener("click",checkingSolution);
+    buttonTwo.addEventListener("click",checkingSolution);
+    buttonThree.addEventListener("click",checkingSolution);
+    buttonFour.addEventListener("click",checkingSolution);
 }
 
 //Timer function
